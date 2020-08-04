4b7c9b7b79749c47b8d54d2a4c080974274683a8
osummero <3515846068@qq.com>
2020-08-04 18:10:24 +0800

    Initial commit

---
+++ app/src/main/java/swu/pjm/douyu/MainActivity.kt
@@ -0,0 +1,11 @@
+package swu.pjm.douyu
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
